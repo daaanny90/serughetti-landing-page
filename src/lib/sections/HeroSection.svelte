70b277d772cdd6c3b9d62a668c6a06ba4b8ce53d
Danny Spina <danny@ostec.de>
2023-03-22 14:21:48 +0100

    Comporess and convert images

--- src/lib/sections/HeroSection.svelte
+++ src/lib/sections/HeroSection.svelte
@@ -9,7 +9,7 @@
       <div class="absolute inset-0">
         <img
           class="h-full w-full object-cover"
-          src="https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+          src="./espowerlifting_hero.webp"
           alt="People working on laptops"
         />
         <div
